3430dd73a59eb418cf9b7f2b24b31737b9c37345
Naim Ayoub <127954922+NaiM-AyouB@users.noreply.github.com>
2025-02-02 22:14:26 +0100

    I learned about Repetition Statment (while)

---
+++ src/RepetitionStatement.java
@@ -0,0 +1,10 @@
+public class RepetitionStatement {
+    public static void main(String[] args) {
+        
+        int count = 10 ;
+        while(++count <=20) {
+            System.out.println(" Hi, I am using while loop");
+        }
+        System.out.println("count = "+count);
+    }
+}
