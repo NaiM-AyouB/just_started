c8ca5c085d7db5740acb60f433d75e65cd4bc927
Naim Ayoub <127954922+NaiM-AyouB@users.noreply.github.com>
2025-02-02 20:25:45 +0100

    I used the switch statement to create a simple Calculator

--- src/TestStatements.java
+++ src/TestStatements.java
@@ -1,5 +1,50 @@
-public class TestStatements {
-    public static void main(String[] args) {
+    /*
+     * if.
+     * if ,else.
+     * if ,else , if.
+     * switch.
+     * "!" in this program I will only use switch statement.
+     */
 
-    }
-}
+     import java.util.Scanner;
+
+     public class TestStatements {
+     
+         public static void main(String[] args) {
+             char c;
+             int x1, x2;
+             
+             try (Scanner in = new Scanner(System.in)) {
+                 System.out.println("Enter one of these operators: *, /, +, -");
+                 c = in.next().charAt(0);
+                 
+                 System.out.println("Enter x1:");
+                 x1 = in.nextInt();
+                 
+                 System.out.println("Enter x2:");
+                 x2 = in.nextInt();  // Fixed typo from x1 to x2
+     
+                 switch (c) {
+                     case '+':
+                         System.out.println("The result = " + (x1 + x2));
+                         break;
+                     case '*':
+                         System.out.println("The result = " + (x1 * x2));
+                         break;
+                     case '/':
+                         if (x2 == 0) {
+                             System.out.println("Error: Division by zero!");
+                         } else {
+                             System.out.println("The result = " + (x1 / x2));
+                         }
+                         break;
+                     case '-':
+                         System.out.println("The result = " + (x1 - x2));
+                         break;
+                     default:
+                         System.out.println("Invalid operator entered!");
+                         break;
+                 }
+             }
+         }
+     }
